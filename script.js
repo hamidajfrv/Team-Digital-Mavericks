3914660542db00a1709d0c72d7843db83931548d
hamidajfrv <110425663+hamidajfrv@users.noreply.github.com>
2023-11-26 23:01:43 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
